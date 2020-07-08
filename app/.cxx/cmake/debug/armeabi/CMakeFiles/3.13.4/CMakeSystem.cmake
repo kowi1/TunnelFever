5402667f84a7b1755810576af542672255dfaaf0
kowi1 <dolukowi@gmail.com>
2020-07-08 16:47:41 +0100

    Initialization

---
+++ app/.cxx/cmake/debug/armeabi/CMakeFiles/3.13.4/CMakeSystem.cmake
@@ -0,0 +1,23 @@
+set(CMAKE_HOST_SYSTEM "Linux-5.3.0-18-generic")
+set(CMAKE_HOST_SYSTEM_NAME "Linux")
+set(CMAKE_HOST_SYSTEM_VERSION "5.3.0-18-generic")
+set(CMAKE_HOST_SYSTEM_PROCESSOR "x86_64")
+
+include("/usr/lib/Android/ndk/build/cmake/android.toolchain.cmake")
+
+set(CMAKE_SYSTEM "Android-14")
+set(CMAKE_SYSTEM_NAME "Android")
+set(CMAKE_SYSTEM_VERSION "14")
+set(CMAKE_SYSTEM_PROCESSOR "armv5te")
+
+set(CMAKE_ANDROID_NDK "/usr/lib/Android/ndk")
+set(CMAKE_ANDROID_STANDALONE_TOOLCHAIN "")
+set(CMAKE_ANDROID_ARCH "arm")
+set(CMAKE_ANDROID_ARCH_ABI "armeabi")
+set(CMAKE_ANDROID_ARCH_HEADER_TRIPLE "arm-linux-androideabi")
+set(CMAKE_ANDROID_NDK_DEPRECATED_HEADERS "0")
+set(CMAKE_ANDROID_ARM_MODE "1")
+
+set(CMAKE_CROSSCOMPILING "TRUE")
+
+set(CMAKE_SYSTEM_LOADED 1)
